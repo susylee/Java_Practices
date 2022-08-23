f941b923d1b22aa8a6d53fafaa7a4a3702c10fe2
Suyeon Lee <lsy76@10.70.208.31>
2022-08-23 14:38:21 -0400

    Inheritance practice

---
+++ Inheritance_PoolArea/src/Rectangle.java
@@ -0,0 +1,33 @@
+
+public class Rectangle {
+	private double width;
+	private double length;
+
+	public Rectangle(double width, double length) {
+		if(width<0) {
+			width = 0;
+		} else {
+			this.width = width;
+		}
+		if(length<0) {
+			length = 0;
+		} else {
+			this.length = length;
+		}
+	} //Rectangle
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getLength() {
+		return length;
+	}
+	public double getArea() {
+		double area;
+		area = width * length;
+		return area;
+	}
+
+
+}
