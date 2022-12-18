5818cacc73093096794ff1f750974d68502aa790
Suyeon Lee <lsy7674@gmail.com>
2022-12-18 00:34:39 -0500

    English-Spanish word dictionary using HashMap put(), get() methods.

---
+++ HashMap_Dictionary/src/Dictionary.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Dictionary {
+
+	public static void main(String[] args) {
+		HashMap<String, String> dicc = new HashMap<String, String>();
+		dicc.put("sky", "cielo");
+		dicc.put("pencil", "lapiz");
+		dicc.put("water", "agua");
+		dicc.put("computer", "computadora");
+		dicc.put("grandmother", "abuela");
+
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.print("Enter the word (q to exit) : "); 
+			String eng = sc.next();
+
+			if(eng.equals("q")) {
+				break;
+			}
+			if(dicc.get(eng) == null) {
+				System.out.println("Incorrect input.");
+			}
+			else {
+				System.out.println("Spanish: " + dicc.get(eng));
+			}
+		}while(true);
+		System.out.println("Program exits...");
+	}
+
+}
